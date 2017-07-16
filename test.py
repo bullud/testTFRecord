392df9e21dfab9ea82a77b1d9f97190e73b31055
bullud <bullud@126.com>
2017-07-16 10:34:01 +0800

    add some modes of tf io

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ num_epochs = 1
 
 batch_size = 1
 
-data_pattern = '/Users/super/yt8m_videofeature/train*.tfrecord'
+num_classes = 4716
+
+data_pattern = '/Users/super/yt8m/train*.tfrecord'
 
 #files = [gfile.Glob(data_pattern[i]) for i in range(len(data_pattern))]
 
@@ -17,7 +19,7 @@ if not files:
 
 logging.info("Number of training files: %s.", str(len(files)))
 
-filename_queue = tf.train.string_input_producer(files, num_epochs=num_epochs, shuffle=True)
+filename_queue = tf.train.string_input_producer(files, num_epochs=num_epochs, shuffle=False)
 
 reader = tf.TFRecordReader()
 
@@ -25,7 +27,7 @@ _, serialized_examples = reader.read_up_to(filename_queue, batch_size)
 
 feature_map = {"video_id": tf.FixedLenFeature([], tf.string),
                "labels": tf.VarLenFeature(tf.int64),
-               "mean_inc3": tf.FixedLenFeature(1024, tf.float32)}
+               "mean_rgb": tf.FixedLenFeature(1024, tf.float32)}
 
 
 features = tf.parse_example(serialized_examples, features=feature_map)
@@ -39,10 +41,20 @@ with tf.Session() as sess:
     threads = tf.train.start_queue_runners(coord=coord)
 
     for i in range(10):
-        #sess.run(features)
-        print(serialized_examples.eval())
-        #sess.run(serialized_examples)
-        #print(len(ex))
+        #print(serialized_examples.eval())
+
+        fea = sess.run(features)
+        vid = fea["video_id"]
+        print(vid)
+
+        lab = fea["labels"]
+        print(lab)
+
+        me = fea["mean_rgb"]
+        print(me)
+
+        lab = tf.sparse_to_indicator(lab, num_classes)
+        print(lab)
 
     coord.request_stop()
     coord.join()
--- testYT8mRR.py
+++ testYT8mRR.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 from tensorflow import gfile
 from tensorflow import logging
 
-data_pattern = ['I:\\testvideo1\\train*.tfrecord', 'I:\\testvideo2\\train*.tfrecord']
+data_pattern = ['/Users/super/yt8m/train*.tfrecord']
 
 num_epochs = 1000
 
-batch_size = 1024
+batch_size = 2
 
-#files = [gfile.Glob(data_pattern[i] for i in range(len(data_pattern)))][0]
+num_classes = 4716
+
 files = gfile.Glob(data_pattern)
 
 print(files)
@@ -18,9 +19,57 @@ if not files:
                   str(data_pattern[i] for i in range(len(data_pattern))) + "'.")
 
 logging.info("Number of training files: %s.", str(len(files)))
-filename_queue = tf.train.string_input_producer(
-    files, num_epochs=num_epochs, shuffle=True)
+
+filename_queue = tf.train.string_input_producer(files, num_epochs=num_epochs, shuffle=True)
 
 reader = tf.TFRecordReader()
 
 _, serialized_examples = reader.read_up_to(filename_queue, num_records=batch_size)
+
+feature_map = {"video_id": tf.FixedLenFeature([], tf.string),
+               "labels": tf.VarLenFeature(tf.int64),
+               "mean_rgb": tf.FixedLenFeature(1024, tf.float32)}
+
+features = tf.parse_example(serialized_examples, features=feature_map)
+
+
+
+init_local_op = tf.initialize_local_variables()
+
+with tf.Session() as sess:
+    sess.run(init_local_op)
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+
+    total_step = 0
+    while total_step < 10:
+        examples = sess.run(serialized_examples)
+        print(examples.shape)
+
+        #feas = sess.run(features)
+        #print(feas['video_id'])
+        #print(feas['labels'])
+
+        #features = tf.parse_example(examples, features=feature_map)
+
+        feas = sess.run(features)
+
+        video_id = feas['video_id']
+        print('video_id = ' + video_id)
+
+        labels_num = len(feas['labels'])
+        print('labels_num = ' + str(labels_num))
+
+        labels = tf.sparse_to_indicator(feas["labels"], num_classes)
+
+        labels.set_shape([None, num_classes])
+
+        labs = sess.run(labels)
+
+        print(labs)
+
+        total_step = total_step + 1
+
+    coord.request_stop()
+    coord.join(threads)
